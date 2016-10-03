00c0871957e44ff2c7da2f0877f882e0d5a35330
Shitao <steven_wst@hotmail.com>
2016-10-03 18:57:58 -0400

    feat: benchmark model and result added
    The benchmark regression model and its result are added.
    The model is based on a random forecast regressor with
    sklearn default parameter settings.

---
+++ code/single_model/base_model.py
@@ -0,0 +1,189 @@
+import datetime
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from scipy.stats import skew
+ 
+def mean_squared_error_(ground_truth, predictions):
+    return mean_squared_error(ground_truth, predictions) ** 0.5
+RMSE = make_scorer(mean_squared_error_, greater_is_better=False)    
+    
+def create_submission(prediction,score):
+    now = datetime.datetime.now()
+    sub_file = 'submission_'+str(score)+'_'+str(now.strftime("%Y-%m-%d-%H-%M"))+'.csv'
+    print ('Creating submission: ', sub_file)
+    pd.DataFrame({'Id': test['Id'].values, 'SalePrice': prediction}).to_csv(sub_file, index=False)
+
+def data_preprocess(train,test):
+#    outlier_idx = [185, 318, 523, 691, 898, 1182]
+#    outlier_idx = [185, 261, 318, 349, 523, 581, 688, 691, 774, 798, 875, 898, 1182, 1256, 1359]
+#    outlier_idx = [66,167,185, 224,261, 318, 349, 523, 581,588, 688, 691, 774, 798, 875, 898,987, 1169,1182, 1256,1298,1324,1359,1442]
+    outlier_idx = [4,11,13,20,46,66,70,167,178,185,199, 224,261, 309,313,318, 349,412,423,440,454,477,478, 523,540, 581,588,595,654,688, 691, 774, 798, 875, 898,926,970,987,1027,1109, 1169,1182,1239, 1256,1298,1324,1353,1359,1405,1442,1447]
+    train.drop(train.index[outlier_idx],inplace=True)
+    all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'],
+                          test.loc[:,'MSSubClass':'SaleCondition']))
+    
+    to_delete = ['Alley','FireplaceQu','PoolQC','Fence','MiscFeature']
+    all_data = all_data.drop(to_delete,axis=1)
+
+    train["SalePrice"] = np.log1p(train["SalePrice"])
+    #log transform skewed numeric features
+    numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+    skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
+    skewed_feats = skewed_feats[skewed_feats > 0.75]
+    skewed_feats = skewed_feats.index
+    all_data[skewed_feats] = np.log1p(all_data[skewed_feats])
+    all_data = pd.get_dummies(all_data)
+    all_data = all_data.fillna(all_data.mean())
+    X_train = all_data[:train.shape[0]]
+    X_test = all_data[train.shape[0]:]
+    y = train.SalePrice
+
+    return X_train,X_test,y
+    
+def model_random_forecast(Xtrain,Xtest,ytrain):
+    
+    X_train = Xtrain
+    y_train = ytrain
+    rfr = RandomForestRegressor(n_jobs=1, random_state=0)
+    param_grid = {}#'n_estimators': [500], 'max_features': [10,15,20,25], 'max_depth':[3,5,7,9,11]}
+    model = GridSearchCV(estimator=rfr, param_grid=param_grid, n_jobs=1, cv=10, scoring=RMSE)
+    model.fit(X_train, y_train)
+    print('Random forecast regression...')
+    print('Best Params:')
+    print(model.best_params_)
+    print('Best CV Score:')
+    print(-model.best_score_)
+
+    y_pred = model.predict(Xtest)
+    return y_pred, -model.best_score_
+
+
+
+def model_gradient_boosting_tree(Xtrain,Xtest,ytrain):
+    
+    X_train = Xtrain
+    y_train = ytrain 
+    gbr = GradientBoostingRegressor(random_state=0)
+    param_grid = {
+        'n_estimators': [500],
+        'max_features': [10,15],
+	'max_depth': [6,8,10],
+        'learning_rate': [0.05,0.1,0.15],
+        'subsample': [0.8]
+    }
+    model = GridSearchCV(estimator=gbr, param_grid=param_grid, n_jobs=1, cv=10, scoring=RMSE)
+    model.fit(X_train, y_train)
+    print('Gradient boosted tree regression...')
+    print('Best Params:')
+    print(model.best_params_)
+    print('Best CV Score:')
+    print(-model.best_score_)
+
+    y_pred = model.predict(Xtest)
+    return y_pred, -model.best_score_
+
+def model_xgb_regression(Xtrain,Xtest,ytrain):
+    
+    X_train = Xtrain
+    y_train = ytrain 
+    
+    xgbreg = xgb.XGBRegressor(seed=0)
+    param_grid = {
+        'n_estimators': [500],
+        'learning_rate': [ 0.05],
+        'max_depth': [ 7, 9, 11],
+        'subsample': [ 0.8],
+        'colsample_bytree': [0.75,0.8,0.85],
+    }
+    model = GridSearchCV(estimator=xgbreg, param_grid=param_grid, n_jobs=1, cv=10, scoring=RMSE)
+    model.fit(X_train, y_train)
+    print('eXtreme Gradient Boosting regression...')
+    print('Best Params:')
+    print(model.best_params_)
+    print('Best CV Score:')
+    print(-model.best_score_)
+
+    y_pred = model.predict(Xtest)
+    return y_pred, -model.best_score_
+
+def model_extra_trees_regression(Xtrain,Xtest,ytrain):
+    
+    X_train = Xtrain
+    y_train = ytrain
+    
+    etr = ExtraTreesRegressor(n_jobs=1, random_state=0)
+    param_grid = {'n_estimators': [500], 'max_features': [10,15,20]}
+    model = GridSearchCV(estimator=etr, param_grid=param_grid, n_jobs=1, cv=10, scoring=RMSE)
+    model.fit(X_train, y_train)
+    print('Extra trees regression...')
+    print('Best Params:')
+    print(model.best_params_)
+    print('Best CV Score:')
+    print(-model.best_score_)
+
+    y_pred = model.predict(Xtest)
+    return y_pred, -model.best_score_
+
+def model_kernel_ridge_regression(Xtrain,Xtest,ytrain):
+    
+    X_train = train[features]
+    y_train = np.log(train['SalePrice'])
+    
+    etr = KernelRidge(kernel = 'linear')
+    param_grid = {'alpha': [1e-2,0.1,0.3,0.5,1,2,4], 'gamma': np.logspace(-3,2,6)}
+    model = GridSearchCV(estimator=etr, param_grid=param_grid, n_jobs=1, cv=10, scoring=RMSE)
+    model.fit(X_train, y_train)
+    print('Kernel ridge regression...')
+    print('Best Params:')
+    print(model.best_params_)
+    print('Best CV Score:')
+    print(-model.best_score_)
+
+    y_pred = model.predict(test[features])
+    return y_pred, -model.best_score_
+
+def model_KNN_regression(Xtrain,Xtest,ytrain):
+    
+    X_train = Xtrain
+    y_train = ytrain 
+    
+    etr = KNeighborsRegressor()
+    param_grid = {'n_neighbors': [3,5,8,10,15]}
+    model = GridSearchCV(estimator=etr, param_grid=param_grid, n_jobs=1, cv=10, scoring=RMSE)
+    model.fit(X_train, y_train)
+    print('KNN regression...')
+    print('Best Params:')
+    print(model.best_params_)
+    print('Best CV Score:')
+    print(-model.best_score_)
+
+    y_pred = model.predict(Xtest)
+    return y_pred, -model.best_score_
+
+
+# read data, build model and do prediction
+train = pd.read_csv("../../input/train.csv") # read train data
+test = pd.read_csv("../../input/test.csv") # read test data
+Xtrain,Xtest,ytrain = data_preprocess(train,test)
+
+
+test_predict,score = model_random_forecast(Xtrain,Xtest,ytrain)
+#test_predict,score = model_xgb_regression(Xtrain,Xtest,ytrain)
+#test_predict,score = model_extra_trees_regression(Xtrain,Xtest,ytrain)
+#test_predict,score = model_gradient_boosting_tree(Xtrain,Xtest,ytrain)
+#test_predict,score = model_KNN_regression(Xtrain,Xtest,ytrain)
+
+
+create_submission(np.exp(test_predict),score)
+
+
+
